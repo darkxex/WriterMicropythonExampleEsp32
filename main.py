d9c8076a580e422a7959e4aaa40e3ec0f73787cd
Angel Toloza <darkxex@gmail.com>
2023-01-16 01:14:00 -0300

    sure

--- main.py
+++ main.py
@@ -1,34 +1,3 @@
-# writer_demo.py Demo pogram for rendering arbitrary fonts to an SSD1306 OLED display.
-# Illustrates a minimal example. Requires ssd1306_setup.py which contains
-# wiring details.
-
-# The MIT License (MIT)
-#
-# Copyright (c) 2018 Peter Hinch
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
-
-# https://learn.adafruit.com/monochrome-oled-breakouts/wiring-128x32-spi-oled-display
-# https://www.proto-pic.co.uk/monochrome-128x32-oled-graphic-display.html
-
-# V0.3 13th Aug 2018
 
 import machine
 from ssd1306_setup import ssd
@@ -36,11 +5,11 @@ from writer import Writer
 
 # Font
 import arial20esp as arial20
-
-wri = Writer(ssd, arial20)
+ssd.fill(0)
+wri = Writer(ssd, arial20, False)
 wri.set_clip(False, False, False)
 Writer.set_textpos(ssd, 0, 0)  # verbose = False to suppress console output
 wri.printstring('Sábado\n')
-wri.printstring('12 Aug 2019\n')
+wri.printstring('16 Dic 2023\n')
 wri.printstring('10.30am')
 ssd.show()
