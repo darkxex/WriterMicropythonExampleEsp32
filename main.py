34e0c9cbe8d158717d14ea5e4cd7d80ab90ba587
Angel Toloza <darkxex@gmail.com>
2023-01-16 00:41:44 -0300

    simple

---
+++ main.py
@@ -0,0 +1,45 @@
+# writer_demo.py Demo pogram for rendering arbitrary fonts to an SSD1306 OLED display.
+# Illustrates a minimal example. Requires ssd1306_setup.py which contains
+# wiring details.
+
+# The MIT License (MIT)
+#
+# Copyright (c) 2018 Peter Hinch
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+# https://learn.adafruit.com/monochrome-oled-breakouts/wiring-128x32-spi-oled-display
+# https://www.proto-pic.co.uk/monochrome-128x32-oled-graphic-display.html
+
+# V0.3 13th Aug 2018
+
+import machine
+from ssd1306_setup import ssd
+from writer import Writer
+
+# Font
+import freesans20
+
+wri = Writer(ssd, freesans20)
+Writer.set_textpos(ssd, 0, 0)  # verbose = False to suppress console output
+wri.printstring('Sunday\n')
+wri.printstring('12 Aug 2018\n')
+wri.printstring('10.30am')
+ssd.show()
